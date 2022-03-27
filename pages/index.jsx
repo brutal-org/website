f5dfb24215116c655f01e9ec7f1f14ba8eef7dc5
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2022-03-27 14:48:43 +0200

    Added support for old articles + working on milestone 4

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,7 @@ import Logo from "../components/common/Logo";
 
 export default function Home() {
   return (
-    <div className="max-w-prose m-auto flex flex-col gap-8 ">
+    <div className="max-w-prose m-auto flex flex-col gap-8 grow">
       <Head>
         <title>BRUTAL</title>
       </Head>
@@ -15,11 +15,18 @@ export default function Home() {
       <p>
         Striking modernist shapes and bold use of modern C are the hallmarks of BRUTAL. Inspired by brutalist design BRUTAL combines the ideals of UNIX from the 1970s with modern technology and engineering.
       </p>
-      <Link href="https://github.com/brutal-org/brutal">
-        <div className="uppercase rounded p-4 bg-black text-white dark:bg-white dark:text-black self-start">
-          contribute on GitHub
-        </div>
-      </Link>
+      <div className="flex gap-4">
+        <Link href="https://github.com/brutal-org/brutal">
+          <div className="uppercase rounded p-4 bg-black text-white dark:bg-white dark:text-black self-start">
+            contribute on GitHub
+          </div>
+        </Link>
+        <Link href="https://github.com/brutal-org/brutal">
+          <div className="uppercase rounded p-4 bg-black text-white dark:bg-white dark:text-black self-start">
+            download the latest release
+          </div>
+        </Link>
+      </div>
     </div>
   )
 }
