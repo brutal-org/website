0982fc4d75ab712c808da35c7a82426ce8b1f835
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2022-03-27 23:21:00 +0200

    Improved templates.

--- components/scafold/Footer.jsx
+++ components/scafold/Footer.jsx
@@ -3,6 +3,7 @@ export function Footer() {
     <div className="font-black">BRUTAL</div>
     <div>"Long live the UNIX Utopia!"</div>
     <div>Copyright © MMXXII The BRUTAL contributors.</div>
+
     <img className="h-10 w-10 m-0 p-0 mt-8" src="/scafold/glider.svg" alt="" />
   </footer>;
 }
