70d9906e170d9e77716ee47dc01e64944cbe9032
Pacifist-Penguin <75931217+Pacifist-Penguin@users.noreply.github.com>
2022-03-31 23:41:51 +0300

    Fixed some of links

--- components/scafold/Navbar.jsx
+++ components/scafold/Navbar.jsx
@@ -3,13 +3,13 @@ import ThemeToggle from "../common/ThemeToggle";
 import Logo from "../common/Logo";
 
 function NavLink(props) {
-  return <Link href={props.href}><div className="uppercase cursor-pointer">{props.text}</div></Link>
+  return <Link href={props.href}><a className="uppercase cursor-pointer">{props.text}</a></Link>
 }
 
 export function Navbar() {
   return <header className="backdrop-blur bg-white/80 dark:bg-black/80  top-0 sticky p-4 mb-8  sep-bottom">
     <div className="flex gap-4 items-center max-w-prose m-auto">
-      <Link href="/"><Logo className="w-8" /></Link>
+      <Link href="/"><a><Logo className="w-8" /></a></Link>
       <NavLink text="about" href="/" />
       <NavLink text="blog" href="/articles" />
       <NavLink text="github" href="https://github.com/brutal-org/brutal" />
